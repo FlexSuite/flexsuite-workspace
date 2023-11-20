5df2d061374a6ea3904f931543b19f9c06326f0b
LucaoMendes <lucas1999mendes@gmail.com>
2023-11-20 12:56:42 -0300

    [Global] Ajustes nos SVGs

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -73,7 +73,7 @@ export function Sidebar({toggleDarkMode}:{toggleDarkMode:()=>void}){
                 <path
                 fill-rule="evenodd"
                 d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
                 ></path>
             </svg>
             </a>
@@ -101,7 +101,7 @@ export function Sidebar({toggleDarkMode}:{toggleDarkMode:()=>void}){
                 <path
                 d="M0 450h7410m0 600H0m0 600h7410m0 600H0m0 600h7410m0 600H0"
                 stroke="#fff"
-                stroke-width="300"
+                strokeWidth="300"
                 />
                 <path fill="#3c3b6e" d="M0 0h2964v2100H0z" />
                 <g fill="#fff">
@@ -150,7 +150,7 @@ export function Sidebar({toggleDarkMode}:{toggleDarkMode:()=>void}){
                         viewBox="0 0 512 512"
                     >
                         <g fill-rule="evenodd">
-                        <g stroke-width="1pt">
+                        <g strokeWidth="1pt">
                             <path
                             fill="#bd3d44"
                             d="M0 0h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0zm0 20h247v10H0z"
@@ -214,7 +214,7 @@ export function Sidebar({toggleDarkMode}:{toggleDarkMode:()=>void}){
                         id="flag-icon-css-it"
                         viewBox="0 0 512 512"
                     >
-                        <g fill-rule="evenodd" stroke-width="1pt">
+                        <g fill-rule="evenodd" strokeWidth="1pt">
                         <path fill="#fff" d="M0 0h512v512H0z" />
                         <path fill="#009246" d="M0 0h170.7v512H0z" />
                         <path fill="#ce2b37" d="M341.3 0H512v512H341.3z" />
