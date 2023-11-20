03fe5ab0ce22c2a273b2e54cb70b270f4abfdde0
LucaoMendes <lucas1999mendes@gmail.com>
2023-11-20 12:57:02 -0300

    [Navbar] Correção de responsividade no menu

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -7,9 +7,9 @@ import { Profile } from './Profile'
 
 export function Navbar() {
     
-    return <nav className='border-b border-gray-200 bg-gray-800 border-gray-700 fixed left-0 right-0 top-0 z-50'>
-                <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
-                    <div className='flex h-16 items-center justify-between'>
+    return <nav className='border-b border-gray-200 px-4 py-2.5 bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50'>
+                <div className='flex flex-wrap justify-between items-center'>
+                    <div className='flex justify-start items-center ps-10'>
                         <div className='flex items-center'>
                             <DrawerNavigation/>
                             <div className='flex-shrink-0 flex items-center'>
@@ -21,19 +21,14 @@ export function Navbar() {
                             </div>
                             <SearchNavbarInput/>
                         </div>
-                        <UserProfile/>
+                    </div>
+                    <div className='flex items-center lg:order-2 mr-20'>
+                        <div className='flex items-center'>
+                            <Notification/>
+                            <Apps/>
+                            <Profile/>
+                        </div>
                     </div>
                 </div>
             </nav>
-
-
-    function UserProfile(){
-        return  <div className='md:block'>
-                    <div className='ml-4 flex items-center md:ml-6'>
-                        <Notification/>
-                        <Apps/>
-                        <Profile/>
-                    </div>
-                </div>
-    }
 }
