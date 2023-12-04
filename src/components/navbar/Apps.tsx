cbc39d7c600ed522659ce7309fd34bf555095dfa
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-04 18:16:07 -0300

    [Navbar] Criação do componente

--- src/components/navbar/Apps.tsx
+++ src/components/navbar/Apps.tsx
@@ -9,7 +9,7 @@ import React from "react"
 function Toggle() {
     return <button
         type="button"
-        data-dropdown-toggle="apps-dropdown"
+        data-dropdown-toggle="modules-dropdown"
         className="p-2 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
     >
         <span className="sr-only">View notifications</span>
@@ -86,13 +86,9 @@ function Dropdown() {
         }
     ]
 
-    return <div
-                className="hidden overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
-                id="apps-dropdown"
-            >
-                <div
-                    className="block py-2 px-4 text-base font-medium text-center text-gray-700 bg-gray-50 dark:bg-gray-600 dark:text-gray-300"
-                >
+    return <div className="hidden overflow-hidden z-50 my-4 max-w-sm text-base list-none bg-white rounded divide-y divide-gray-100 shadow-lg dark:bg-gray-700 dark:divide-gray-600 rounded-xl"
+                id="modules-dropdown">
+                <div className="block py-2 px-4 text-base font-medium text-center text-gray-700 bg-gray-50 dark:bg-gray-600 dark:text-gray-300">
                     Modulos
                 </div>
                 <div className="grid grid-cols-3 gap-4 p-4">
