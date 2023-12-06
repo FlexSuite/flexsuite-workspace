22d9e1462267fa6badcfb6e3810c73ca3bdb71c6
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-05 23:00:22 -0300

    [FlexSuiteNavigation] Incorporação do contexto de navegação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,27 @@ import React from 'react'
 import { Navbar } from '@/components/navbar'
 import '../styles/globals.css'
 import { Sidebar } from '@/components/sidebar'
-import 'flowbite'
+import { FlexSuiteNavigation } from '@/context/FlexSuiteNavigation'
 
 function MyApp({ Component, pageProps }: any) {
+
   const [isDarkMode, setIsDarkMode] = React.useState(true)
 
   const toggleDarkMode = () => setIsDarkMode(!isDarkMode)
 
-
-  return <div className={`antialiased bg-gray-50 dark:bg-gray-900 ${isDarkMode?'dark':'light'}`}>
-          <Navbar/>
-          <Sidebar toggleDarkMode={toggleDarkMode}/>
-          <div className="p-4 sm:ml-64 pt-20">
-            <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-                <Component {...pageProps} />
-            </div>
-          </div>
-        </div>
+  return  (
+            <FlexSuiteNavigation.Provider>
+              <div className={`antialiased bg-gray-50 dark:bg-gray-900 ${isDarkMode?'dark':'light'}`}>
+                <Navbar/>
+                <Sidebar toggleDarkMode={toggleDarkMode}/>
+                <div className="p-4 sm:ml-64 pt-20">
+                  <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+                      <Component {...pageProps} />
+                  </div>
+                </div>
+              </div>
+            </FlexSuiteNavigation.Provider>
+          )
 }
 
 export default MyApp
