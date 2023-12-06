eeeaa806eeb2c7a6dcf0b12fa962bd68e38e30b8
LucaoMendes <lucas1999mendes@gmail.com>
2023-12-05 22:59:05 -0300

    [Navbar] Init FLowbite

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,11 +1,18 @@
-import Image from 'next/image'
+import { useEffect } from 'react'
 import { SearchNavbarInput } from './SearchNavbarInput'
 import { Notification } from './Notification'
 import { Apps } from './Apps'
 import { DrawerNavigation } from './DrawerNavigation'
 import { Profile } from './Profile'
+import { initFlowbite } from 'flowbite'
+import Image from 'next/image'
 
 export function Navbar() {
+
+    useEffect(()=> {
+        initFlowbite()
+    })
+
     
     return <nav className='border-b border-gray-200 px-4 py-2.5 bg-gray-800 dark:border-gray-700 fixed left-0 right-0 top-0 z-50'>
                 <div className='flex flex-wrap justify-between items-center'>
